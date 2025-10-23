4c4e844312febfaa95a98aceed5c7d64e1d0d452
Michail19 <Mi.ershov.2017@yandex.ru>
2025-10-23 21:48:00 +0300

    Repository update

--- SubscriptionService/src/main/java/com/ms/subscriptionservice/repository/SubscriptionRepository.java
+++ SubscriptionService/src/main/java/com/ms/subscriptionservice/repository/SubscriptionRepository.java
@@ -1,4 +1,11 @@
 package com.ms.subscriptionservice.repository;
 
-public class SubscriptionRepository {
+import com.ms.subscriptionservice.model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    Optional<Subscription> findById(Long employeeId);
+
 }
