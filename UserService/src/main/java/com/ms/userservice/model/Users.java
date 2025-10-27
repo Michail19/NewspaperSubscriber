21ff19330b8c1ff961c8e27eddd2f1d28e88abdd
Michail19 <Mi.ershov.2017@yandex.ru>
2025-10-27 21:16:30 +0300

    Model user maked

--- UserService/src/main/java/com/ms/userservice/model/Users.java
+++ UserService/src/main/java/com/ms/userservice/model/Users.java
@@ -1,21 +1,35 @@
 package com.ms.userservice.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 @Setter
 @Getter
+@Entity
+@Table(name = "users")
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "surname")
     private String surname;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "patronymic")
     private String patronymic;
+
+    @Column(name = "age")
     private int age;
+
+    @Column(name = "registration_date")
     private Timestamp registration_date;
-    private List<Integer> subscriptions;
 
     public Users(long id, String surname, String name, String patronymic, int age, Timestamp registration_date) {
         this.id = id;
