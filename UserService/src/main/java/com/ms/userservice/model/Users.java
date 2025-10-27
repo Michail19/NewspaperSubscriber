949bbf45ee4aff8afac33171123f0cbcd9d84f38
Michail19 <Mi.ershov.2017@yandex.ru>
2025-10-27 18:25:01 +0300

    Update Users.java

--- UserService/src/main/java/com/ms/userservice/model/Users.java
+++ UserService/src/main/java/com/ms/userservice/model/Users.java
@@ -1,4 +1,30 @@
 package com.ms.userservice.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Setter
+@Getter
 public class Users {
+    private long id;
+    private String surname;
+    private String name;
+    private String patronymic;
+    private int age;
+    private Timestamp registration_date;
+    private List<Integer> subscriptions;
+
+    public Users(long id, String surname, String name, String patronymic, int age, Timestamp registration_date) {
+        this.id = id;
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.age = age;
+        this.registration_date = registration_date;
+    }
+
+    public Users() {}
 }
