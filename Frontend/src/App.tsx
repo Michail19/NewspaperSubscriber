dbee462ea56132f44ccff234327ce08f2de9794a
Michail19 <Mi.ershov.2017@yandex.ru>
2025-11-23 21:10:56 +0300

    Working update

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavBar } from "./components/NavBar";
 import { CatalogsPage } from "./pages/CatalogsPage";
 import { CategoriesPage } from "./pages/CategoriesPage";
@@ -6,22 +6,25 @@ import { SeriesPage } from "./pages/SeriesPage";
 import { UserPage } from "./pages/UserPage";
 import { SubscriptionsPage } from "./pages/SubscriptionsPage";
 
-
 export default function App() {
-  const route = window.location.hash;
+    const [route, setRoute] = useState(window.location.hash || "#/");
 
+    useEffect(() => {
+        const handler = () => setRoute(window.location.hash || "#/");
+        window.addEventListener("hashchange", handler);
+        return () => window.removeEventListener("hashchange", handler);
+    }, []);
 
-  let Page = CatalogsPage;
-  if (route.startsWith("#/categories")) Page = CategoriesPage;
-  if (route.startsWith("#/series")) Page = SeriesPage;
-  if (route.startsWith("#/user")) Page = UserPage;
-  if (route.startsWith("#/subscriptions")) Page = SubscriptionsPage;
+    let Page = CatalogsPage;
+    if (route.startsWith("#/categories")) Page = CategoriesPage;
+    if (route.startsWith("#/series")) Page = SeriesPage;
+    if (route.startsWith("#/user")) Page = UserPage;
+    if (route.startsWith("#/subscriptions")) Page = SubscriptionsPage;
 
-
-  return (
-      <div className="min-h-screen bg-gray-100">
-        <NavBar />
-        <Page />
-      </div>
-  );
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <NavBar />
+            <Page />
+        </div>
+    );
 }
--- Frontend/src/graphql/client.js
+++ Frontend/src/graphql/client.js
@@ -1,5 +1,5 @@
 export async function graphqlRequest(query, variables = {}) {
-    const response = await fetch("http://localhost:8080/graphql", {
+    const response = await fetch("http://apigateway:8080/graphql", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ query, variables })
