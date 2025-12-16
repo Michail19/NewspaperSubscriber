b86ae47a1b619be2bd504602ef8a68ea4e7c4340
Michail19 <Mi.ershov.2017@yandex.ru>
2025-12-16 22:21:22 +0300

    Update WebClientConfig.java

--- APIGateway/src/main/java/com/ms/apigateway/config/WebClientConfig.java
+++ APIGateway/src/main/java/com/ms/apigateway/config/WebClientConfig.java
@@ -2,13 +2,13 @@ package com.ms.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;  // вместо WebClient
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
